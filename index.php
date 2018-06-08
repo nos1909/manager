b88f229644f338423650d1f17a1e3e21eb4c5cd6
nos1909 <39665683+nos1909@users.noreply.github.com>
2018-06-08 17:30:42 +0700

    Update index.php

--- index.php
+++ index.php
@@ -12,6 +12,7 @@ session_start();
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
   <script src="http://code.jquery.com/jquery-1.12.0.min.js"></script>
+  <link rel="stylesheet" href="../css/sweetalert.css">
   <style type="text/css">
   	.panel-primary>.panel-heading {
   	color: #f00;
@@ -28,6 +29,11 @@ session_start();
     background-color: rgba(255,255,255,.1);
     border-color: #4cae4c;
 }
+.btn-warning {
+    color: #fd9866;
+    background-color: rgba(255,255,255,.1);
+    border-color: #eea236;
+}
   </style>
 
 </head>
@@ -36,8 +42,8 @@ session_start();
 <div class="container">
 	<br /><br />
 <div class="panel panel-primary">
-      <div class="panel-heading"><b>Cài Đặt Bot</b></div>
-      <div class="panel-body">      
+      <div class="panel-heading"><b>Menu Cài Đặt Bot Miễn Phí - NosApp</b></div>
+      <div class="panel-body"><center><button type="button" class="btn btn-danger" data-toggle="modal" data-target="#lay_token">Hướng Dẫn Lấy Token</button>   <button type="button" class="btn btn-success" data-toggle="modal" data-target="#lay_id_sticker">Hướng Dẫn Lấy ID Sticker</button>   <button type="button" class="btn btn-warning" data-toggle="modal" data-target="#cron_job">Hướng Dẫn Cron Job</button></center><hr>    
 <form method="post" id="form_login">
 <div class="form-group">
   <label for="token">Token:</label>
@@ -88,7 +94,7 @@ $(document).ready(function()
          
         //kiem tra xem da nhap tai khoan chua
         if(token == ''){
-            alert('Vui lòng nhập Token');
+            swal("Thông Báo!", "Bạn chưa nhập token!");
             return false;
         }
          
@@ -103,7 +109,7 @@ $(document).ready(function()
                {                       
                     if(data == 'false')
                     {
-                        alert('Token die! Vui lòng cập nhật lại!');
+                        swal("Thông Báo!", "Token Die! Vui lòng cập nhật lại!");
                     }else{
                         $('#content').html(data);
                     }
@@ -121,5 +127,71 @@ $(document).ready(function()
 </div>
     </div>
 
+
+<!-- Modal Lay Token -->
+<div id="lay_token" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">Hướng Dẫn Lấy Token Trên Máy Tính</h4>
+      </div>
+      <div class="modal-body">
+        <p>Some text in the modal.</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+
+  </div>
+</div>
+
+<!-- Modal Lay ID Sticker -->
+<div id="lay_id_sticker" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">Modal Header</h4>
+      </div>
+      <div class="modal-body">
+        <p>Some text in the modal.</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+
+  </div>
+</div>
+
+<!-- Modal Cron Job -->
+<div id="cron_job" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">Modal Header</h4>
+      </div>
+      <div class="modal-body">
+        <p>Some text in the modal.</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+
+  </div>
+</div>
+
+<script src="../js/sweetalert.js"></script>
+<script src="../js/sweetalert.custom.js"></script>
 </body>
 </html>
